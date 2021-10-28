4f55970e56debf62ae01cffa891983c49a0e8330
mynameisnish <nishanttyagi390@gmail.com>
2021-10-28 10:53:51 +0530

    Update Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -48,6 +48,8 @@ namespace src
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                
+                
             }
 
             app.UseHttpsRedirection();
